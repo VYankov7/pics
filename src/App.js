8b0a0ca816995a86ff521850572b365f200c5674
VYankov7 <vyankov02@gmail.com>
2023-02-26 22:03:14 +0200

    Images-Searching-Application

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import searchImages from "./api";
+import { useState } from 'react';
 import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
+import searchImages from "./api";
 
 function App() {
-  const handleSubmit = (term) => {
-    console.log(`Do a search with`, term);
-    // searchImages()
-    
+  const [images, setImages] = useState([]);
+
+  const handleSubmit = async (term) => {
+  const result = await searchImages(term)
+
+   setImages(result)
   }
 
 
   return <div>
     <SearchBar onSubmit={handleSubmit} />
+    <ImageList images={images}/>
   </div>
 }
 
