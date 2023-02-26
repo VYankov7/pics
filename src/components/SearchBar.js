6cf7cd7262580bc122fd3995435544beac567874
VYankov7 <vyankov02@gmail.com>
2023-02-26 11:27:29 +0200

    random pictures from web api

---
+++ src/components/SearchBar.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+function SearchBar({ onSubmit }) {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    onSubmit('cars')
+  }
+
+ return (
+  <div>
+    <form onSubmit={handleFormSubmit}>
+    <input />
+    </form>
+  </div>
+ );
+}
+
+export default SearchBar;
